619e07f155f8e1c771056eca4fecd2a2f72f73fb
Arnab Datta <dattaarnab2013@gmail.com>
2026-02-24 10:00:00 +0530

    custom react library

---
+++ customreact.js
@@ -0,0 +1,10 @@
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https//google.com",
+    target: "_blank",
+  },
+  Children: "Click me to visit google",
+};
+
+const mainContainer = document.querySelector("#root");
