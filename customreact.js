5626251075a8a11461042b9548976bd35e0d1c33
Arnab Datta <dattaarnab2013@gmail.com>
2026-02-24 10:00:00 +0530

    customRender func v1

--- customreact.js
+++ customreact.js
@@ -1,10 +1,20 @@
+function customRender(reactElement, container) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+  domElement.setAttribute("href", reactElement.props.href);
+  domElement.setAttribute("target", reactElement.props.target);
+  container.appendChild(domElement);
+}
+
 const reactElement = {
   type: "a",
   props: {
-    href: "https//google.com",
+    href: "https://www.google.com/",
     target: "_blank",
   },
-  Children: "Click me to visit google",
+  children: "Click me to visit google",
 };
 
 const mainContainer = document.querySelector("#root");
+
+customRender(reactElement, mainContainer);
